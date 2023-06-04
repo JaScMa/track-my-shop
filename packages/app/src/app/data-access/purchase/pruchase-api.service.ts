4a486636e3013fe79206373d6a624ce30895b3bd
JaScMa <63072931+JaScMa@users.noreply.github.com>
2023-06-04 13:59:36 +0200

    local proxy

--- packages/app/src/app/data-access/purchase/pruchase-api.service.ts
+++ packages/app/src/app/data-access/purchase/pruchase-api.service.ts
@@ -1,17 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Purchase } from '@prisma/client';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PruchaseApiService {
-
   getPurchases() {
-    return this.http.get<Purchase[]>('api/purchase/all');
+    return this.http.get<Purchase[]>(environment.backendUrl + '/purchase/all');
   }
 
-  constructor(
-    private http: HttpClient,
-  ) { }
+  constructor(private http: HttpClient) {}
 }
